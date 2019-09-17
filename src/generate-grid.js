a9eaee74b50df4704b59b2cccfd0f24df125163e
Chris Valleskey <cvalleskey@gmail.com>
2019-09-17 09:58:03 -0500

    Appcast

--- src/generate-grid.js
+++ src/generate-grid.js
@@ -92,214 +92,210 @@ export function onShutdown() {
 
 function generateGrid(settings) {
 
-    var settings = {...defaults, ...settings };
+  var settings = {...defaults, ...settings };
 
-    log('settings')
-    log(settings)
+  var document = Document.getSelectedDocument();
+  let page = document.selectedPage;
+  var selection = document.selectedLayers;
+  var selected = (selection.length == 1)? selection.layers[0] : false;
 
-    var document = Document.getSelectedDocument();
-    let page = document.selectedPage;
-    var selection = document.selectedLayers;
-    var selected = (selection.length == 1)? selection.layers[0] : false;
+  if(!selected) {
+    getWebview(webviewIdentifier).close();
+    UI.message('Select a layer or artboard to generate a grid.');
+    return;
+  }
 
-    if(!selected) {
-      getWebview(webviewIdentifier).close();
-      UI.message('Select a layer or artboard to generate a grid.');
+  var columns = settings.columns;
+  var gutter = settings.gutter;
+  var gutterSize = settings.gutterSize;
+  var margin = settings.margin;
+  var marginSize = settings.marginSize;
+
+  // Math to change breakpoint if artboard is selected vsettings. object, and if the size is smaller than the max
+  if(settings.breakpoint == "auto") {
+    var breakpoint = settings.breakpoint;
+  } else {
+    var breakpoint = parseInt(settings.breakpoint, 10);
+  }
+
+  if(selected) {
+    if(breakpoint == "auto") {
+      breakpoint = selected.frame.width;
+    }
+  }
+
+  if(Number(breakpoint) > settings.containerWidth) {
+    breakpoint = settings.containerWidth;
+  }
+
+  // Calculate margin width based on number or percent
+  var pixelMarginSize = marginSize;
+  if(typeof marginSize == "string" && marginSize.includes('%')) {
+    pixelMarginSize = breakpoint * (marginSize.replace("%", "") / 100);
+  } else {
+    pixelMarginSize = parseFloat(marginSize, 10);
+  }
+  if(margin == 0) {
+    pixelMarginSize = 0;
+  }
+
+  // Calculate main container space, minus margin on both sides
+  let containerFinalWidth = breakpoint - (pixelMarginSize * 2);
+
+  // Calculate single gutter width
+  if(typeof gutterSize == "string") {
+    var pixelGutterSize = containerFinalWidth * (gutterSize.replace("%", "") / 100);
+  } else {
+    var pixelGutterSize = gutterSize;
+  }
+  if(gutter == 0) {
+    pixelGutterSize = 0;
+  }
+
+  // Calculate single column width
+  let pixelColumnWidth = parseFloat(((containerFinalWidth - (pixelGutterSize)*(settings.columnCount-1)) / settings.columnCount).toFixed(2), 10);
+
+  var columnsArray = [];
+
+  if(columns == "auto") {
+
+    if(selected.frame) {
+
+      var selectedWidth = selected.frame.width;
+
+      if(selectedWidth < 600) {
+        columns = [12];
+      } else {
+        columns = Array(12).fill(1);
+      }
+
+    } else {
+      UI.message('To use auto, please select an artboard.');
       return;
     }
 
-    var columns = settings.columns;
-    var gutter = settings.gutter;
-    var gutterSize = settings.gutterSize;
-    var margin = settings.margin;
-    var marginSize = settings.marginSize;
+  } else if(columns == "1") {
+    columns = [12];
+  } else if(columns == "2") {
+		columns = [6,6];
+  } else if(columns == "3") {
+		columns = [4,4,4];
+  } else if(columns == "4") {
+		columns = [3,3,3,3];
+  } else if(columns == "6") {
+		columns = [2,2,2,2,2,2];
+  } else if(columns == "12") {
+		columns = Array(12).fill(1);
+  } else if(columns == "8,4") {
+    columns = [8,4];
+  } else if(columns == "3,9") {
+    columns = [3,9];
+  }
 
-    // Math to change breakpoint if artboard is selected vsettings. object, and if the size is smaller than the max
-    if(settings.breakpoint == "auto") {
-      var breakpoint = settings.breakpoint;
-    } else {
-      var breakpoint = parseInt(settings.breakpoint, 10);
-    }
+  for(var i = 0; i < columns.length; i++) {
 
-    if(selected) {
-      if(breakpoint == "auto") {
-        breakpoint = selected.frame.width;
-      }
-    }
+		var column = columns[i];
 
-    if(Number(breakpoint) > settings.containerWidth) {
-      breakpoint = settings.containerWidth;
-    }
-
-    // Calculate margin width based on number or percent
-    var pixelMarginSize = marginSize;
-    if(typeof marginSize == "string" && marginSize.includes('%')) {
-      pixelMarginSize = breakpoint * (marginSize.replace("%", "") / 100);
-    } else {
-      pixelMarginSize = parseFloat(marginSize, 10);
-    }
-    if(margin == 0) {
-      pixelMarginSize = 0;
-    }
-
-    // Calculate main container space, minus margin on both sides
-    let containerFinalWidth = breakpoint - (pixelMarginSize * 2);
-
-    // Calculate single gutter width
-    // 1140 * 2%
-    if(typeof gutterSize == "string") {
-      var pixelGutterSize = containerFinalWidth * (gutterSize.replace("%", "") / 100);
-    } else {
-      var pixelGutterSize = gutterSize;
-    }
-    if(gutter == 0) {
-      pixelGutterSize = 0;
-    }
-
-    // Calculate single column width
-    let pixelColumnWidth = parseFloat(((containerFinalWidth - (pixelGutterSize)*(settings.columnCount-1)) / settings.columnCount).toFixed(2), 10);
-
-    var columnsArray = [];
-
-    if(columns == "auto") {
-
-      if(selected.frame) {
-
-        var selectedWidth = selected.frame.width;
-
-        if(selectedWidth < 600) {
-          columns = [12];
-        } else {
-          columns = Array(12).fill(1);
-        }
-
-      } else {
-        UI.message('To use auto, please select an artboard.');
-        return;
-      }
-
-    } else if(columns == "1") {
-      columns = [12];
-    } else if(columns == "2") {
-			columns = [6,6];
-    } else if(columns == "3") {
-			columns = [4,4,4];
-    } else if(columns == "4") {
-			columns = [3,3,3,3];
-    } else if(columns == "6") {
-			columns = [2,2,2,2,2,2];
-    } else if(columns == "12") {
-			columns = Array(12).fill(1);
-    } else if(columns == "8,4") {
-      columns = [8,4];
-    } else if(columns == "3,9") {
-      columns = [3,9];
-    }
-
-    for(var i = 0; i < columns.length; i++) {
-
-  		var column = columns[i];
-
-      // Add outer margin
-      if(i == 0 && margin) {
-        columnsArray.push({
-          type: 'margin',
-          width : pixelMarginSize
-        });
-      }
-
-      // Add gutter
-      if(i != 0 && gutter) {
-        columnsArray.push({
-          type: 'gutter',
-          width : pixelGutterSize
-        });
-      }
-      // Add column
+    // Add outer margin
+    if(i == 0 && margin) {
       columnsArray.push({
-        type: 'column',
-        width: Number(((column-1) * pixelGutterSize + column * pixelColumnWidth).toFixed(2)) // pixelMarginSize
+        type: 'margin',
+        width : pixelMarginSize
       });
-
-      // Add outer margin
-      if(i == (columns.length-1) && margin) {
-        columnsArray.push({
-          type: 'margin',
-          width : pixelMarginSize
-        });
-      }
     }
 
-    var grids = {
-      name : '📐 Grid',
-      frame : {
-        width : breakpoint,
-        height : (selection.length == 1)? selected.frame.height : 100
-      }
+    // Add gutter
+    if(i != 0 && gutter) {
+      columnsArray.push({
+        type: 'gutter',
+        width : pixelGutterSize
+      });
     }
+    // Add column
+    columnsArray.push({
+      type: 'column',
+      width: Number(((column-1) * pixelGutterSize + column * pixelColumnWidth).toFixed(2)) // pixelMarginSize
+    });
 
-    if(selected.type == "Artboard") {
-      grids.parent = selected;
+    // Add outer margin
+    if(i == (columns.length-1) && margin) {
+      columnsArray.push({
+        type: 'margin',
+        width : pixelMarginSize
+      });
+    }
+  }
+
+  var grids = {
+    name : '📐 Grid',
+    frame : {
+      width : breakpoint,
+      height : (selection.length == 1)? selected.frame.height : 100
+    }
+  }
+
+  if(selected.type == "Artboard") {
+    grids.parent = selected;
+    grids.frame.x = (selected.frame.width - breakpoint) / 2;
+    // gridsettings.frame.y = 0;
+  } else if(selected.type == "ShapePath") {
+    if(breakpoint >= selected.frame.width) {
+      grids.frame.x = selected.frame.x;
+    } else {
       grids.frame.x = (selected.frame.width - breakpoint) / 2;
-      // gridsettings.frame.y = 0;
-    } else if(selected.type == "ShapePath") {
-      if(breakpoint >= selected.frame.width) {
-        grids.frame.x = selected.frame.x;
-      } else {
-        grids.frame.x = (selected.frame.width - breakpoint) / 2;
-      }
-      grids.frame.y = selected.frame.y;
-      if(selected.getParentArtboard() == undefined) {
-        grids.parent = page;
-      } else {
-        //gridsettings.parent = selected.getParentArtboard();
-        grids.parent = selected.parent;
-      }
     }
+    grids.frame.y = selected.frame.y;
+    if(selected.getParentArtboard() == undefined) {
+      grids.parent = page;
+    } else {
+      //gridsettings.parent = selected.getParentArtboard();
+      grids.parent = selected.parent;
+    }
+  }
 
-    var gridGroup = new Document.Group(grids);
+  var gridGroup = new Document.Group(grids);
 
-    var _x = 0;
-    var _y = 0;
+  var _x = 0;
+  var _y = 0;
 
-    for(var x = 0; x < columnsArray.length; x++) {
+  for(var x = 0; x < columnsArray.length; x++) {
 
-      var el = columnsArray[x];
+    var el = columnsArray[x];
 
-      var shape = new Document.ShapePath({
-          parent: gridGroup,
-          name : el.type,
-          frame: {
-            x: _x,
-            y: _y,
-            width: el.width,
-            height: (selection.length == 1)? selection.layers[0].frame.height : 100
-          },
-          style: { fills: [config.colors.column] }
-      });
+    var shape = new Document.ShapePath({
+        parent: gridGroup,
+        name : el.type,
+        frame: {
+          x: _x,
+          y: _y,
+          width: el.width,
+          height: (selection.length == 1)? selection.layers[0].frame.height : 100
+        },
+        style: { fills: [config.colors.column] }
+    });
 
-      if(el.type == "gutter" || el.type == "margin") {
-        shape.style.fills = [
-          {
-            fillType : Document.Style.FillType.Pattern,
-            pattern : {
-              patternType : Document.Style.PatternFillType.Tile,
-              tileScale : 0.25,
-              image : { "base64": config.patterns[el.type]}
-              //image : loadLocalImage("/Users/chrisettings.valleskey/Documents/github/intouch-toolkit/intouch-toolkit.sketchplugin/Contents/Resources/shade-" + el.type + ".png"),
-            }
+    if(el.type == "gutter" || el.type == "margin") {
+      shape.style.fills = [
+        {
+          fillType : Document.Style.FillType.Pattern,
+          pattern : {
+            patternType : Document.Style.PatternFillType.Tile,
+            tileScale : 0.25,
+            image : { "base64": config.patterns[el.type]}
+            //image : loadLocalImage("/Users/chrisettings.valleskey/Documents/github/intouch-toolkit/intouch-toolkit.sketchplugin/Contents/Resources/shade-" + el.type + ".png"),
           }
-        ];
-        shape.style.innerShadows = [
-          { color : config.colors[el.type], x : 1, y : 0, blur : 0, spread : 0 },
-          { color : config.colors[el.type], x : -1, y : 0, blur : 0, spread : 0 }
-        ];
-      }
-
-      _x += el.width;
+        }
+      ];
+      shape.style.innerShadows = [
+        { color : config.colors[el.type], x : 1, y : 0, blur : 0, spread : 0 },
+        { color : config.colors[el.type], x : -1, y : 0, blur : 0, spread : 0 }
+      ];
     }
 
-    gridGroup.adjustToFit();
+    _x += el.width;
+  }
+
+  gridGroup.adjustToFit();
 }
 
 export function makeGridOne()       { generateGrid({ columns: "1" }); }
