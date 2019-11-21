66e582a97cf8fa9b2d58455eac37b8e00a0b9c4a
Chris Valleskey <cvalleskey@gmail.com>
2019-11-21 09:45:17 -0600

    colors works!

--- resources/tokens.js
+++ resources/tokens.js
@@ -2,11 +2,14 @@ document.addEventListener('contextmenu', (e) => { e.preventDefault() })
 
 document.getElementById('confirm').addEventListener('click', () => {
   var elTokens = document.getElementById('tokens');
-  window.postMessage('updateTokens', {
-    styles : JSON.parse(elTokens.value)
-  });
+  //window.postMessage('nativeLog', "sending tokens");
+  window.postMessage('updateTokens', { tokens: JSON.parse(elTokens.value) });
 })
 
 window.getTokens = (tokens) => {
-  document.getElementById('tokens').innerHTML = JSON.stringify(tokens,null,'\t')
+  //window.postMessage('nativeLog', "getting tokens");
+  //window.postMessage('nativeLog', tokens);
+  //if(tokens.length) {
+    document.getElementById('tokens').innerHTML = "[" + JSON.stringify(tokens,null,'\t') + "]"
+  //}
 }
--- src/tokens.js
+++ src/tokens.js
@@ -1,63 +1,85 @@
-import BrowserWindow from 'sketch-module-web-view'
-import { getWebview } from 'sketch-module-web-view/remote'
-import UI from 'sketch/ui'
-import Document from 'sketch/dom'
+import BrowserWindow from "sketch-module-web-view";
+import { getWebview } from "sketch-module-web-view/remote";
+import UI from "sketch/ui";
+import Document from "sketch/dom";
+import Settings from "sketch/settings";
+import { Page, Style, SharedStyle } from 'sketch/dom';
 
 const webViewOptions = {
-  identifier: 'intouch-toolkit.tokens',
+  identifier: "intouch-toolkit.tokens",
   webpage: "tokens.html",
   width: 480,
   height: 370,
   show: false,
-  title: 'Tokens'
-}
+  title: "Tokens"
+};
 
-export default function () {
+const defaults = {
+  colors: [],
+  gradients: [],
+  typography: [],
+  spacing: []
+};
 
-  log('wtf')
-
+export default function() {
+  const document = Document.getSelectedDocument();
   const browserWindow = new BrowserWindow(webViewOptions);
-  browserWindow.once('ready-to-show', () => {
-    var tokens = getToken();
-    browserWindow.webContents.executeJavaScript(`getTokens(${JSON.stringify(tokens)})`)
-    browserWindow.show()
-  })
+  browserWindow.once("ready-to-show", () => {
+    var tokens = getTokens();
+    browserWindow.show();
+    browserWindow.webContents.executeJavaScript(`getTokens(${JSON.stringify(tokens)})`).then(res => {});
+  });
   const webContents = browserWindow.webContents;
-
-  webContents.on('updateTokens', props => updateTokens(props));
-
-  browserWindow.loadURL(require('../resources/' + webViewOptions.webpage))
+  webContents.on("updateTokens", tokens => {
+    updateTokens(tokens);
+    getWebview(webViewOptions.identifier).close();
+  });
+  webContents.on("nativeLog", s => log(s));
+  browserWindow.loadURL(require("../resources/" + webViewOptions.webpage));
 
   function getTokens() {
-    if(Settings.documentSettingForKey(document, 'tokens') == undefined) {
+    //log('getTokens')
+    //log(Settings.documentSettingForKey(document, "tokens"))
+    if (Settings.documentSettingForKey(document, "tokens") == undefined) {
       return [];
     } else {
-      return Settings.documentSettingForKey(document, 'tokens');
+      return Settings.documentSettingForKey(document, "tokens");
     }
   }
 
-  function updateTokens(tokens) {
-
-    var document = Document.getSelectedDocument();
-    var styles = document.sharedTextStyles;
-
-    for(var i = 0; i < styles.length; i++) {
-      if(styles[i].name == name) {
-        return styles[i];
+  function updateTokens(response) {
+    //log('tokens')
+    //log(tokens)
+    var tokens = response.tokens[0];
+    document.colors = [];
+    tokens.colors.forEach(color => {
+      document.colors.push({ color: color.color, name: color.name });
+      var existingSharedLayerStyle = document.sharedLayerStyles.filter(
+        style => style.name == "color/" + color.name
+      );
+      log(existingSharedLayerStyle)
+      if (existingSharedLayerStyle.length) {
+        existingSharedLayerStyle[0].style.fills = [
+          { color: color.color, fillType: Style.FillType.Color }
+        ];
+      } else {
+        document.sharedLayerStyles.push({
+          name: "color/" + color.name,
+          style: {
+            fills: [{ color: color.color, fillType: Style.FillType.Color }]
+          }
+        });
       }
-    }
+    });
 
-    UI.message('Text Styles created successfully.')
-    getWebview(webViewOptions.identifier).close();
-
-    return null;
+    Settings.setDocumentSettingForKey(document, 'tokens', tokens);
+    UI.message("Tokens updated successfully.");
   }
-
 }
 
 export function onShutdown() {
-  const existingWebview = getWebview(webViewOptions.identifier)
+  const existingWebview = getWebview(webViewOptions.identifier);
   if (existingWebview) {
-    existingWebview.close()
+    existingWebview.close();
   }
 }
