61761f8a53f4bbafd8e2ed57dfd24823bbe45513
Chris Valleskey <cvalleskey@gmail.com>
2020-04-21 16:30:32 -0500

    basic tab stop support

--- resources/set-tab-stops.js
+++ resources/set-tab-stops.js
@@ -13,30 +13,41 @@ document.addEventListener('keypress', function(e) {
 var defaultTabStops = [16,32,48,64,80,96,112,128,144,160,176,192,208];
 
 document.getElementById('button').addEventListener('click', () => {
-  var tabStopsArray = [
-    parseInt(document.getElementById('tabInterval0').value) || defaultTabStops[0],
-    parseInt(document.getElementById('tabInterval1').value) || defaultTabStops[1],
-    parseInt(document.getElementById('tabInterval2').value) || defaultTabStops[2],
-    parseInt(document.getElementById('tabInterval3').value) || defaultTabStops[3],
-    parseInt(document.getElementById('tabInterval4').value) || defaultTabStops[4],
-    parseInt(document.getElementById('tabInterval5').value) || defaultTabStops[5],
-    parseInt(document.getElementById('tabInterval6').value) || defaultTabStops[6],
-    parseInt(document.getElementById('tabInterval7').value) || defaultTabStops[7],
-    parseInt(document.getElementById('tabInterval8').value) || defaultTabStops[8],
-    parseInt(document.getElementById('tabInterval9').value) || defaultTabStops[9],
-    parseInt(document.getElementById('tabInterval10').value) || defaultTabStops[10],
-    parseInt(document.getElementById('tabInterval11').value) || defaultTabStops[11],
-    parseInt(document.getElementById('tabInterval12').value) || defaultTabStops[12]
-  ]
+
+  var tabStopsArray = [];
+  defaultTabStops.forEach((stop, i) => {
+    tabStopsArray.push(parseInt(document.getElementById('tabInterval' + i).value) || defaultTabStops[i])
+  });
+  // var tabStopsArray = [
+  //   parseInt(document.getElementById('tabInterval0').value) || defaultTabStops[0],
+  //   parseInt(document.getElementById('tabInterval1').value) || defaultTabStops[1],
+  //   parseInt(document.getElementById('tabInterval2').value) || defaultTabStops[2],
+  //   parseInt(document.getElementById('tabInterval3').value) || defaultTabStops[3],
+  //   parseInt(document.getElementById('tabInterval4').value) || defaultTabStops[4],
+  //   parseInt(document.getElementById('tabInterval5').value) || defaultTabStops[5],
+  //   parseInt(document.getElementById('tabInterval6').value) || defaultTabStops[6],
+  //   parseInt(document.getElementById('tabInterval7').value) || defaultTabStops[7],
+  //   parseInt(document.getElementById('tabInterval8').value) || defaultTabStops[8],
+  //   parseInt(document.getElementById('tabInterval9').value) || defaultTabStops[9],
+  //   parseInt(document.getElementById('tabInterval10').value) || defaultTabStops[10],
+  //   parseInt(document.getElementById('tabInterval11').value) || defaultTabStops[11],
+  //   parseInt(document.getElementById('tabInterval12').value) || defaultTabStops[12]
+  // ]
   window.postMessage('update', { tabStops: tabStopsArray });
 })
 
-// call the wevbiew from the plugin
+document.getElementById('autoStop').addEventListener('click', () => {
+  defaultTabStops.forEach((stop, i) => {
+    document.getElementById('tabInterval' + i).value = Math.round((16 * i * (window.fontSize / 16)) + window.fontSize);
+  })
+})
+
 window.setValues = (obj) => {
-  window.postMessage('nativeLog', 'nativeLog');
-  window.postMessage('nativeLog', obj.tabStops);
+  // window.postMessage('nativeLog', 'nativeLog');
+  // window.postMessage('nativeLog', obj.tabStops);
   var tabStops = obj.tabStops.split(',')
   tabStops.forEach((stop, i) => {
     document.getElementById('tabInterval' + i).value = Number(stop);
   });
+  window.fontSize = obj.fontSize || 16;
 }
--- src/set-tab-stops.js
+++ src/set-tab-stops.js
@@ -18,13 +18,16 @@ const webviewIdentifier = 'intouch-toolkit.webview'
 
 export default function (context) {
 
+  let document = Document.getSelectedDocument();
+  let selectedLayers = document.selectedLayers;
+
   const webViewOptions = {
     identifier: webviewIdentifier,
     webpage: "set-tab-stops.html",
     width: 660,
     height: 184,
     show: false,
-    title: 'Set Tab Stops',
+    title: 'Set List Spacing',
     parent: Document.getSelectedDocument(),
     center: true
   }
@@ -35,18 +38,18 @@ export default function (context) {
 
     var selectedTabStops = getSelectedLayerTabStops();
 
-    console.log('toString()')
-    console.log(selectedTabStops.toString())
     var tabStopsArray = [];
     selectedTabStops.forEach((stop, i) => {
       tabStopsArray.push(stop);
     });
-    console.log("new tabStopsArray")
-    console.log(tabStopsArray)
 
+    console.log("fontSize: " + selectedLayers.layers[0].style.fontSize);
 
     browserWindow.webContents
-      .executeJavaScript(`setValues(${JSON.stringify({ tabStops: tabStopsArray.toString() })})`)
+      .executeJavaScript(`setValues(${JSON.stringify({
+        tabStops: tabStopsArray.toString(),
+        fontSize: selectedLayers.layers[0].style.fontSize
+      })})`)
       .catch(error => log(error))
   });
 
@@ -74,66 +77,23 @@ function update(obj) {
 
   var newTabStops = obj.tabStops;
 
-  console.log('obj.tabStops')
-  console.log(obj.tabStops)
-
-  if(newTabStops == "auto") {
-    // set bullet spacing based on font size and a good harmony
-  }
-
   let document = Document.getSelectedDocument();
   let selectedLayers = document.selectedLayers;
 
   selectedLayers.forEach((layer) => {
     var currentTabStops = layer.sketchObject.attributedStringValue().attributeForKey("NSParagraphStyle").treeAsDictionary().style.tabStops;
-    // console.log('currentTabStops')
-    // console.log(currentTabStops)
+
+    // Convert object to array and change values from strings to numbers
     var tabIntervalArray = Object.values(newTabStops);
     tabIntervalArray.forEach((item) => { item = parseInt(item); })
-    //var tabIntervalArray = [16,32,48,64,80,96,112,128,144,160,176,192,208];
 
-    // var newTabStopsArray = [];
-    // tabIntervalArray.forEach((stop, i) => {
-    //   newTabStopsArray.push(NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, stop));
-    // });
-
+    // Apply the new stops to the Text layer
     var paragraphStyle = layer.sketchObject.attributedStringValue().attributeForKey("NSParagraphStyle");
-
     tabIntervalArray.forEach((stop, i) => {
       paragraphStyle.tabStops()[i] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, stop);
     })
-    // paragraphStyle.tabStops()[0] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[0]);
-    // paragraphStyle.tabStops()[1] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[1]);
-    // paragraphStyle.tabStops()[2] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[2]);
-    // paragraphStyle.tabStops()[3] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[3]);
-    // paragraphStyle.tabStops()[4] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[4]);
-    // paragraphStyle.tabStops()[5] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[5]);
-    // paragraphStyle.tabStops()[6] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[6]);
-    // paragraphStyle.tabStops()[7] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[7]);
-    // paragraphStyle.tabStops()[8] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[8]);
-    // paragraphStyle.tabStops()[9] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[9]);
-    // paragraphStyle.tabStops()[10] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[10]);
-    // paragraphStyle.tabStops()[11] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[11]);
-    // paragraphStyle.tabStops()[12] = NSTextTab.alloc().initWithType_location(NSTextAlignmentLeft, tabIntervalArray[12]);
     paragraphStyle.headIndent = tabIntervalArray[1];
-
-    //paragraphStyle.tabStops()[0] = newTabStopsArray[0];
-    // paragraphStyle.tabStops()[1] = newTabStopsArray[1];
-    // paragraphStyle.tabStops()[2] = newTabStopsArray[2];
-    // paragraphStyle.tabStops()[3] = newTabStopsArray[3];
-    // paragraphStyle.tabStops()[4] = newTabStopsArray[4];
-    // paragraphStyle.tabStops()[5] = newTabStopsArray[5];
-    // paragraphStyle.tabStops()[6] = newTabStopsArray[6];
-    // paragraphStyle.tabStops()[7] = newTabStopsArray[7];
-    // paragraphStyle.tabStops()[8] = newTabStopsArray[8];
-    // paragraphStyle.tabStops()[9] = newTabStopsArray[9];
-    // paragraphStyle.tabStops()[10] = newTabStopsArray[10];
-    // paragraphStyle.tabStops()[11] = newTabStopsArray[11];
-    // paragraphStyle.tabStops()[12] = newTabStopsArray[12];
-    //paragraphStyle.headIndent = newTabStopsArray[1];
-
-    console.log('final tabStops')
-    console.log(paragraphStyle.tabStops())
+    layer.sketchObject.adjustFrameToFit();
 
   });
 
@@ -142,9 +102,6 @@ function update(obj) {
 function getSelectedLayerTabStops() {
   let document = Document.getSelectedDocument();
   let selectedLayers = document.selectedLayers;
-
-  //console.log("selectedLayers.layers[0].sketchObject")
-  //console.log(selectedLayers.layers[0].sketchObject)
 
   var selectedTabStops = selectedLayers.layers[0].sketchObject.attributedStringValue().attributeForKey("NSParagraphStyle").treeAsDictionary().style.tabStops;
 
